81494f15714e2e32768f7d032871c2b90fa0bdf8
manikdevbhagat <manikdevbhagat@gmail.com>
2023-10-14 13:01:25 +0530

    Added link to header logo

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -58,7 +58,13 @@ const Header = () => {
         <div className="flex items-center justify-between">
           {/*Logo*/}
           <div>
-            <img src={logo} alt="" className="max-w-max w-[150px] md:w-[200px] h-auto" />
+            <Link to="/home">
+              <img
+                src={logo}
+                alt=""
+                className="max-w-max w-[150px] md:w-[200px] h-auto"
+              />
+            </Link>
           </div>
           {/*Menu*/}
           <div className="navigation" ref={menuRef} onClick={toggleMenu}>
