6b2a20cb29420320a68ea3518dd0c22301aea6bf
manikdevbhagat <manikdevbhagat@gmail.com>
2023-10-17 11:15:19 +0530

    Added swiper slides in a map function

--- frontend/src/components/Testimonial/Testimonial.tsx
+++ frontend/src/components/Testimonial/Testimonial.tsx
@@ -1,113 +1,53 @@
 import React from "react";
-// import { Pagination } from "swiper/modules";
-import { EffectCards} from 'swiper/modules';
+import { EffectCards } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
-import 'swiper/css/effect-cards';
+import "swiper/css/effect-cards";
+import "./styles.css";
 import testimonialImg from "../../assets/images/testimonial.jpeg";
 import { HiStar } from "react-icons/hi";
 
 const Testimonial = () => {
   return (
-    <div className="mt-[30px] mx-[30px] lg:mt-[55px]">
+    <div className="testimonial-container mt-[30px] max-w-[290px] md:max-w-[400px] lg:mt-[55px]">
       <Swiper
+        className="swiper-testimonial"
         modules={[EffectCards]}
-        effect={'cards'}
+        effect={"cards"}
         grabCursor={true}
       >
-        <SwiperSlide>
-          <div className="flex flex-col justify-center items-center py-[30px] px-5 rounded-[13px]">
-            <div className="flex items-center gap-[13px]">
-              <div>
-                <h4 className="text-[18px] leading-[30px] font-semiBold text-headingColor">
-                  Manik Bhagat
-                </h4>
-                <div className="flex items-center gap-[2px]">
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
+        {Array.from({ length: 5 }).map((_) => {
+        return (
+          <SwiperSlide className="swiper-slide-testimonial">
+            <div className="flex flex-col justify-center items-center py-[30px] px-5 rounded-[13px]">
+              <div className="flex items-center gap-[13px]">
+                <div>
+                  <h4 className="text-[18px] leading-[30px] font-semiBold text-headingColor">
+                    Manik Bhagat
+                  </h4>
+                  <div className="flex justify-center items-center gap-[2px]">
+                    <HiStar className="text-yellowColor w-[18px] h-5" />
+                    <HiStar className="text-yellowColor w-[18px] h-5" />
+                    <HiStar className="text-yellowColor w-[18px] h-5" />
+                    <HiStar className="text-yellowColor w-[18px] h-5" />
+                    <HiStar className="text-yellowColor w-[18px] h-5" />
+                  </div>
                 </div>
               </div>
+              <p className="text-[16px] text-center leading-7 mt-4 text-textColor font-[400]">
+                "I never thought achieving my fitness goals could be this easy.
+                HealthiVerse made it possible."
+              </p>
+              <img
+                className="rounded-xl mt-[20px]"
+                src={testimonialImg}
+                alt=""
+              />
             </div>
-            <p className="text-[16px] text-center leading-7 mt-4 text-textColor font-[400]">
-              "I never thought achieving my fitness goals could be this easy.
-              HealthiVerse made it possible."
-            </p>
-            <img className="rounded-xl mt-[20px]" src={testimonialImg} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex flex-col justify-center items-center py-[30px] px-5 rounded-[13px]">
-            <div className="flex items-center gap-[13px]">
-              <div>
-                <h4 className="text-[18px] leading-[30px] font-semiBold text-headingColor">
-                  Manik Bhagat
-                </h4>
-                <div className="flex items-center gap-[2px]">
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                </div>
-              </div>
-            </div>
-            <p className="text-[16px] text-center leading-7 mt-4 text-textColor font-[400]">
-              "I never thought achieving my fitness goals could be this easy.
-              HealthiVerse made it possible."
-            </p>
-            <img className="rounded-xl mt-[20px]" src={testimonialImg} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex flex-col justify-center items-center py-[30px] px-5 rounded-[13px]">
-            <div className="flex items-center gap-[13px]">
-              <div>
-                <h4 className="text-[18px] leading-[30px] font-semiBold text-headingColor">
-                  Manik Bhagat
-                </h4>
-                <div className="flex items-center gap-[2px]">
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                </div>
-              </div>
-            </div>
-            <p className="text-[16px] text-center leading-7 mt-4 text-textColor font-[400]">
-              "I never thought achieving my fitness goals could be this easy.
-              HealthiVerse made it possible."
-            </p>
-            <img className="rounded-xl mt-[20px]" src={testimonialImg} alt="" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="flex flex-col justify-center items-center py-[30px] px-5 rounded-[13px]">
-            <div className="flex items-center gap-[13px]">
-              <div>
-                <h4 className="text-[18px] leading-[30px] font-semiBold text-headingColor">
-                  Manik Bhagat
-                </h4>
-                <div className="flex items-center gap-[2px]">
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                  <HiStar className="text-yellowColor w-[18px] h-5" />
-                </div>
-              </div>
-            </div>
-            <p className="text-[16px] text-center leading-7 mt-4 text-textColor font-[400]">
-              "I never thought achieving my fitness goals could be this easy.
-              HealthiVerse made it possible."
-            </p>
-            <img className="rounded-xl mt-[20px]" src={testimonialImg} alt="" />
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+        );
+      })}
       </Swiper>
     </div>
   );
