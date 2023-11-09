5074c22b5558296578b8b01eba95a9c2caa4a082
manikdevbhagat <manikdevbhagat@gmail.com>
2023-11-09 14:14:19 +0530

    Updated client testimonial

--- frontend/src/components/Testimonial/Testimonial.tsx
+++ frontend/src/components/Testimonial/Testimonial.tsx
@@ -16,38 +16,39 @@ const Testimonial = () => {
         modules={[EffectCards]}
         effect={"cards"}
         grabCursor={true}
+        initialSlide={2}
       >
         {Array.from({ length: 5 }).map((_) => {
-        return (
-          <SwiperSlide className="swiper-slide-testimonial">
-            <div className="flex flex-col justify-center items-center py-[30px] px-5 rounded-[13px]">
-              <div className="flex items-center gap-[13px]">
-                <div>
-                  <h4 className="text-[18px] leading-[30px] font-semiBold text-headingColor">
-                    Manik Bhagat
-                  </h4>
-                  <div className="flex justify-center items-center gap-[2px]">
-                    <HiStar className="text-yellowColor w-[18px] h-5" />
-                    <HiStar className="text-yellowColor w-[18px] h-5" />
-                    <HiStar className="text-yellowColor w-[18px] h-5" />
-                    <HiStar className="text-yellowColor w-[18px] h-5" />
-                    <HiStar className="text-yellowColor w-[18px] h-5" />
+          return (
+            <SwiperSlide className="swiper-slide-testimonial">
+              <div className="flex flex-col justify-center items-center py-[30px] px-5 rounded-[13px]">
+                <div className="flex items-center gap-[13px]">
+                  <div>
+                    <h4 className="text-[18px] leading-[30px] font-semiBold text-headingColor">
+                      Manik Bhagat
+                    </h4>
+                    <div className="flex justify-center items-center gap-[2px]">
+                      <HiStar className="text-yellowColor w-[18px] h-5" />
+                      <HiStar className="text-yellowColor w-[18px] h-5" />
+                      <HiStar className="text-yellowColor w-[18px] h-5" />
+                      <HiStar className="text-yellowColor w-[18px] h-5" />
+                      <HiStar className="text-yellowColor w-[18px] h-5" />
+                    </div>
                   </div>
                 </div>
+                <p className="text-[16px] text-center leading-7 mt-4 text-textColor font-[400]">
+                  "I never thought achieving my fitness goals could be this
+                  easy. HealthiVerse made it possible."
+                </p>
+                <img
+                  className="rounded-xl mt-[20px]"
+                  src={testimonialImg}
+                  alt=""
+                />
               </div>
-              <p className="text-[16px] text-center leading-7 mt-4 text-textColor font-[400]">
-                "I never thought achieving my fitness goals could be this easy.
-                HealthiVerse made it possible."
-              </p>
-              <img
-                className="rounded-xl mt-[20px]"
-                src={testimonialImg}
-                alt=""
-              />
-            </div>
-          </SwiperSlide>
-        );
-      })}
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </div>
   );
