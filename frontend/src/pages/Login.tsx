b1066801806a064d14f3da22989d1cfc3fe2a6aa
manikdevbhagat <manikdevbhagat@gmail.com>
2023-11-10 12:10:11 +0530

    Removed login button disabled logic

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -104,7 +104,6 @@ const Login = () => {
 
               <div className="mt-7">
                 <button
-                  disabled={loading && true}
                   type="submit"
                   className="py-3 px-4  bg-primaryColor hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
                 >
